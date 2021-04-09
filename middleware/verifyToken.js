555756af279f0c160e17a79d3d4af43c0e82c199
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2021-04-09 23:36:14 +0600

    middleware for token verfication

---
+++ middleware/verifyToken.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+function auth(res, res, next) {
+	const token = req.header("auth-token");
+	if (!token) return res.status(400).send("Access Denied");
+	try {
+		const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+		req.user = verified;
+	} catch (error) {
+		return res.status(400).send("Invalid Token");
+	}
+}
+
+module.exports = auth;
